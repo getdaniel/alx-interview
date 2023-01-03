1c56b85dd745c07064f3786709dbdd3d323d9736
Me <getdaniel>
2023-01-03 21:58:08 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """Return True if all boxes can be opened, else return False."""
+    n = len(boxes)
+    opened_boxes = set([0])
+    unoppened_boxes = set(boxes[0]).difference(set([0]))
+
+    while len(unoppened_boxes) > 0:
+        box_key = unoppened_boxes.pop()
+
+        if not box_key or box_key >= n or box_key < 0:
+            continue
+        if box_key not in opened_boxes:
+            unoppened_boxes = unoppened_boxes.union(boxes[box_key])
+            opened_boxes.add(box_key)
+
+    return n == len(opened_boxes)
