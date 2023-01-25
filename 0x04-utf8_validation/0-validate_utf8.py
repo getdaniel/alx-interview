309d0d2ea6359d736fab774ed6b4ee9c439e1876
Me <getdaniel>
2023-01-25 23:14:33 +0300

    refactor the code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,22 @@ def validUTF8(data):
     """
         Determines if a given data set represents a valid UTF-8 encoding.
     """
-    try:
-        decoded_data = bytes(data).decode('utf-8')
-        return True
-    except:
-        return False
+    i = 0
+    while i < len(data):
+        byte_count = 0
+        if (data[i] & 128) == 0:
+            byte_count = 1
+        elif (data[i] & 224) == 192:
+            byte_count = 2
+        elif (data[i] & 240) == 224:
+            byte_count = 3
+        elif (data[i] & 248) == 240:
+            byte_count = 4
+        else:
+            return False
+
+        for j in range(1, byte_count):
+            if i+j >= len(data) or (data[i+j] & 192) != 128:
+                return False
+        i += byte_count
+    return True
