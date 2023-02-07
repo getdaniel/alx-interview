0aeeb38ca7b6ffced77febcb0abb369a901cdcd1
Me <getdaniel>
2023-02-07 12:58:31 +0300

    refactore to fix the error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
+const filmId = process.argv[2];
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}`;
+const baseUrl = `https://swapi.dev/api/films/${filmId}/`;
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const film = JSON.parse(body);
-    film.characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(`${character.name}`);
-        }
-      });
-    });
+request(baseUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
 });
