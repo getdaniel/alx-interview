22824c594aa585d32f9538abac06749b6eddd7e3
Me <getdaniel>
2023-03-07 10:20:30 +0300

    try to test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,44 +14,40 @@ def isWinner(x, nums):
     - Name of the player that won the most rounds.
       If the winner cannot be determined, return None.
     """
-    # Helper function to check if a number is prime
-    def is_prime(n):
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    def sieve(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                for j in range(i ** 2, n + 1, i):
+                    primes[j] = False
+        return [i for i in range(2, n + 1) if primes[i]]
 
-    # Function to play a single game
     def play_game(n):
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        player = 0  # Maria goes first, so player 0 is Maria and player 1 is Ben
+        primes = sieve(n)
+        current_player = "Maria"
         while primes:
-            p = primes[0]  # Choose the smallest prime
-            if player == 0:
-                player = 1
-            else:
-                player = 0
-            for i in range(p, n + 1, p):
-                if i in primes:
-                    primes.remove(i)
-        return player
+            found_move = False
+            for p in primes:
+                if n % p == 0:
+                    found_move = True
+                    primes.remove(p)
+                    current_player = "Ben" if current_player == "Maria" else "Maria"
+                    break
+            if not found_move:
+                break
+        return current_player
 
-    # Play x rounds of the game and keep track of the winners
-    winners = {"Maria": 0, "Ben": 0}
+    maria_wins = ben_wins = 0
     for n in nums:
         winner = play_game(n)
-        if winner is not None:
-            if winner == 0:
-                winners["Maria"] += 1
-            else:
-                winners["Ben"] += 1
-
-    # Determine the overall winner
-    if winners["Maria"] > winners["Ben"]:
+        if winner == "Maria":
+            maria_wins += 1
+        elif winner == "Ben":
+            ben_wins += 1
+    if maria_wins > ben_wins:
         return "Maria"
-    elif winners["Maria"] < winners["Ben"]:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
