977a1f98d1aeae06e24d7421b718e49da65b45a4
Me <getdaniel>
2023-03-07 10:29:42 +0300

    refactor the code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,40 +14,32 @@ def isWinner(x, nums):
     - Name of the player that won the most rounds.
       If the winner cannot be determined, return None.
     """
-    def sieve(n):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i ** 2, n + 1, i):
-                    primes[j] = False
-        return [i for i in range(2, n + 1) if primes[i]]
-
-    def play_game(n):
-        primes = sieve(n)
-        current_player = "Maria"
-        while primes:
-            found_move = False
-            for p in primes:
-                if n % p == 0:
-                    found_move = True
-                    primes.remove(p)
-                    current_player = "Ben" if current_player == "Maria" else "Maria"
-                    break
-            if not found_move:
-                break
-        return current_player
-
-    maria_wins = ben_wins = 0
+    wins = {"Maria": 0, "Ben": 0}
     for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-    if maria_wins > ben_wins:
+        # initialize a list of booleans to keep track of which numbers are prime
+        primes = [True] * (n+1)
+        primes[0] = primes[1] = False
+        # mark all multiples of primes as composite
+        for i in range(2, int(n**0.5)+1):
+            if primes[i]:
+                for j in range(i*i, n+1, i):
+                    primes[j] = False
+        turn = 0  # 0 for Maria, 1 for Ben
+        while True:
+            # find the smallest remaining prime
+            p = next((p for p in range(2, n+1) if primes[p]), None)
+            if not p:
+                # no more primes left, the current player loses
+                wins["Ben" if turn == 0 else "Maria"] += 1
+                break
+            # remove p and its multiples from the set
+            for i in range(p, n+1, p):
+                primes[i] = False
+            # switch to the other player's turn
+            turn = 1 - turn
+    if wins["Maria"] > wins["Ben"]:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif wins["Maria"] < wins["Ben"]:
         return "Ben"
     else:
         return None
